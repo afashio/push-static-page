415c5e081c14e77eb74b033d0b72b1a98ca7f583
afashio <push.webstudio@gmail.com>
2020-07-22 01:34:56 +0300

    Added migrations

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# push-static-page
+# Push static page
+
+### Instalation
+
+1. Install via composer  
+`composer require afashio/push-static-page `
+
+2. Run migrations  
+`php yii migrate/up --migrationPath=@vendor/afashio/push-static-page/src/migrations`  
+`php yii migrate/up --migrationPath=@vendor/costa-rico/yii2-images/migrations`
+
---
+++ src/migrations/m191120_141420_create_page_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%page}}`.
+ */
+class m191120_141420_create_page_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%page}}', [
+            'id' => $this->primaryKey(),
+            'status' => $this->integer(1),
+            'is_main' => $this->integer(1)->defaultValue(0),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%page}}');
+    }
+}
