3ea86b34177727978ac79638c06bbbd383cf6af0
afashio <push.webstudio@gmail.com>
2020-07-22 18:37:32 +0300

    Fixed page_lang migration

--- src/migrations/m191120_141904_create_page_lang_table.php
+++ src/migrations/m191120_141904_create_page_lang_table.php
@@ -4,9 +4,6 @@ use yii\db\Migration;
 
 /**
  * Handles the creation of table `{{%page_lang}}`.
- * Has foreign keys to the tables:
- *
- * - `{{%page}}`
  */
 class m191120_141904_create_page_lang_table extends Migration
 {
@@ -15,31 +12,14 @@ class m191120_141904_create_page_lang_table extends Migration
      */
     public function safeUp()
     {
-        $this->createTable('{{%page_lang}}', [
+        $this->createTable(\afashio\pages\models\PageLang::tableName(), [
             'id' => $this->primaryKey(),
             'page_id' => $this->integer(),
             'language' => $this->string(6),
             'title' => $this->string(250),
             'text' => $this->text(),
-            'slug' => $this->string(250)->unique(),
         ]);
 
-        // creates index for column `page_id`
-        $this->createIndex(
-            '{{%idx-page_lang-page_id}}',
-            '{{%page_lang}}',
-            'page_id'
-        );
-
-        // add foreign key for table `{{%page}}`
-        $this->addForeignKey(
-            '{{%fk-page_lang-page_id}}',
-            '{{%page_lang}}',
-            'page_id',
-            '{{%page}}',
-            'id',
-            'CASCADE'
-        );
     }
 
     /**
@@ -47,18 +27,6 @@ class m191120_141904_create_page_lang_table extends Migration
      */
     public function safeDown()
     {
-        // drops foreign key for table `{{%page}}`
-        $this->dropForeignKey(
-            '{{%fk-page_lang-page_id}}',
-            '{{%page_lang}}'
-        );
-
-        // drops index for column `page_id`
-        $this->dropIndex(
-            '{{%idx-page_lang-page_id}}',
-            '{{%page_lang}}'
-        );
-
-        $this->dropTable('{{%page_lang}}');
+        $this->dropTable(\afashio\pages\models\PageLang::tableName());
     }
 }
